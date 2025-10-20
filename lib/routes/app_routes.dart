1df7f1f872d3bb0d3f35cac27192ec8e00e64287
Pelangi <pelangi@Pelangis-MacBook-Pro.local>
2025-10-20 09:26:14 +0700

    addig routes

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,17 @@
+part of 'app_pages.dart';
+
+
+abstract class Routes {
+  Routes._();
+
+  static const SPLASH = _paths.SPLASH;
+  static const HOME = _paths.HOME;
+  static const NEWS_DETAIL = _paths.NEWS_DETAIL;
+}
+
+abstract class _paths {
+  _paths._();
+  static const SPLASH = '/splash';
+  static const HOME = '/home';
+  static const NEWS_DETAIL = '/news_detail';
+}
