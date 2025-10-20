046ec541e381adce44d884c05f9cf37d9314d468
Pelangi <pelangi@Pelangis-MacBook-Pro.local>
2025-10-20 10:17:00 +0700

    finishing splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:tribun_app/utils/app_colors.dart';
-
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
@@ -10,55 +9,54 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> 
-with SingleTickerProviderStateMixin{
+ with SingleTickerProviderStateMixin {
   late AnimationController _animationController;
   late Animation<double> _fadeAnimation;
   late Animation<double> _scaleAnimation;
 
-  @override
+  @override 
   void initState() {
-    super.initState();
+   super.initState();
     _animationController = AnimationController(
       duration: Duration(seconds: 2),
-      vsync: this
+      vsync: this,
     );
 
     _fadeAnimation = Tween<double>(
-      begin: 0.0,
+      begin: 0.5,
       end: 1.0,
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeInOut,
+    ));
+    _scaleAnimation = Tween<double>(
+      begin: 0.5,
+      end: 1.0,
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.elasticOut,
+    ));
 
-      ).animate(CurvedAnimation(parent: _animationController, curve: Curves.easeInOut));
+    _animationController.forward();
 
-      _scaleAnimation= Tween<double>(
-        begin: 0.5, 
-        end: 1.0
-        ).animate(CurvedAnimation(parent: _animationController, curve: Curves.easeInOut));
-
-
-        _animationController.forward();
-
-        //navigate to home screen after .... 3 sec
-        Future.delayed(Duration(seconds: 3), () {
-          //TODO: definding all route for each screens
-          // Get.offAllNamed(Route.HOME)
-        });
-
+    // // Navigate to home after 3 seconds
+    //  Future.delayed(Duration(seconds: 3), () {
+    //   Get.offAllNamed(Routes.home);
+    // });
   }
-
   @override
   void dispose() {
     _animationController.dispose();
     super.dispose();
   }
 
-
   @override
-  Widget build(BuildContext context) {
+   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.primary,
       body: Center(
         child: AnimatedBuilder(
-          animation: _animationController, 
+          animation: _animationController,
           builder: (context, child) {
             return FadeTransition(
               opacity: _fadeAnimation,
@@ -68,8 +66,8 @@ with SingleTickerProviderStateMixin{
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Container(
-                      width: 10,
-                      height: 20,
+                      width: 120,
+                      height: 120,
                       decoration: BoxDecoration(
                         color: Colors.white,
                         borderRadius: BorderRadius.circular(20),
@@ -78,20 +76,44 @@ with SingleTickerProviderStateMixin{
                             color: Colors.black.withValues(alpha: 0.2),
                             blurRadius: 20,
                             offset: Offset(0, 10),
-                          )
-                        ]
+                          ),
+                        ],
                       ),
-                      //TODO: ICON News paper
-                    )
+                      child: Icon(
+                        Icons.newspaper,
+                        size: 60,
+                        color: AppColors.primary,
+                      ),
+                    ),
+                    SizedBox(height: 30),
+                    Text(
+                      'News App',
+                      style: TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        letterSpacing: 1.5,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'Stay Updated with Latest News',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.white.withValues( alpha: 0.8),
+                      ),
+                    ),
+                    SizedBox(height: 50),
+                    CircularProgressIndicator(
+                      valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                    ),
                   ],
                 ),
-                ),
-              );
-
-          }
-          ),
+              ),
+            );
+          },
+        ),
       ),
     );
-    
-  }
-}
+   }
+ }
